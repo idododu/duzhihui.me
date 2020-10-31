17eb223659a578174e58a861ee4c05785f7a00d6
idododu <doddu@qq.com>
2020-10-31 16:48:35 +0800

    Update tbcat.js

--- tools/tbcat.js
+++ tools/tbcat.js
@@ -32,34 +32,6 @@ function getCaptureImg() {
     }
 }
 
-// 进入养猫
-function enterCat() {
-    click("养猫分20亿");
-    toast("准备养猫");
-    sleep(3*sleepTimes);
-    return true;
-}
-
-// 领猫币
-function getCoin() {
-    toast("准备领猫币");
-    click("领猫币");
-    sleep(sleepTimes);
-}
-
-// 进入总排行榜
-function enterRank() {
-    toast("进入排行榜");
-    click("总排行榜");
-    sleep(sleepTimes);
-
-    toast("查看更多好友");
-    click("查看更多好友");
-    sleep(sleepTimes);
-    return true;
-}
-
-
 // 结束后返回主页面
 function whenComplete() {
     toast("结束");
@@ -74,15 +46,113 @@ function openTaobao() {
     sleep(2*sleepTimes);
     // 打开我的淘宝
     click(894,2154,1050,2310);
-    sleep(2*sleepTimes);
+    sleep(3*sleepTimes);
     return true;
 }
 
+// 进入养猫
+function enterCat() {
+    toast("点击养猫分20亿");
+    click(353, 1639, 491,1777)
+    sleep(2*sleepTimes);
+    // toast("升级猫猫");
+    // click(0, 1734,303,2025)
+    // sleep(2*sleepTimes)
+    toast("每天猫币点击领取");
+    console.log('.............')
+    if(textContains('喵币点击领取').exists()) {
+        textContains('喵币点击领取').click()
+        console.log('111111')
+    }
+    console.log('2222222')
+    
+    
+    // if(btn.exists()) {
+    //     btn.click()
+    //     sleep(2*sleepTimes)
+    // }
+    
+    return true;
+}
+
+// 领猫币
+function getCoin() {
+    toast("领猫币");
+    click('赚喵币');
+    sleep(2*sleepTimes);
+
+    // 签到
+    toast("签到");
+    btn = text('签到').findOnce(0)
+    if(btn) {
+        btn.click(sleep(2*sleepTimes))
+    }
+    
+    let btn
+    //  巴巴农场
+    toast("去领取");
+    click('去领取')
+    sleep(3*sleepTimes);
+    click('点击领取')
+    sleep(2*sleepTimes);
+
+    
+    let start = 0
+    while(true) {
+        btn = text('去浏览').findOnce(start)
+        if(!btn) {
+            btn = text('去逛逛').findOnce(start)
+        }
+        if(!btn) {
+            btn = text('去完成').findOnce(start)
+        }
+        if(!btn) {
+            btn = text('去搜索').findOnce(start)
+        }
+        if(btn) {
+            btn.click()
+            sleep(3*sleepTimes)
+            swipe(100, 100, 110, 500, 500)
+            sleep(5*sleepTimes)
+            swipe(110, 500, 100, 800, 500)
+            sleep(5*sleepTimes)
+            swipe(110, 800, 100, 500, 500)
+            sleep(5*sleepTimes)
+            back()
+            sleep(3*sleepTimes)
+        } else {
+            break;
+        }
+    }
+
+    while(true) {
+        btn = text('领取奖励').findOnce(start)
+        if(btn) {
+            btn.click()
+            sleep(3*sleepTimes)
+        } else {
+            break;
+        }
+    }
+
+    // click('去支付宝领更多喵币')
+    // sleep(5*sleepTimes)
+    // btn = id('alimod-2020-1111-game').findOnce(0)
+    // if(btn) btn.click()
+    // back()
+    // back()
+    text('关闭').click()
+}
+
 function feed() {
-    toast("准备喂猫");
-    for(var i = 0; i < 2; i++) {
-        click("喂猫领红包");
-        sleep(sleepTimes);
+    toast("喂猫领红包");
+    // text('返回我的猫').click()
+    // sleep(sleepTimes)
+    let i = 0
+    while(i<100) {
+        click(483, 1635, 597, 1749)
+        sleep(1*sleepTimes)
+        i++
     }
 }
 //程序主入口
